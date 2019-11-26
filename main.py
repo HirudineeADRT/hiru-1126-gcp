21a77ec2ee69e670f4795a054b371315969795ea
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-26 11:10:47 +0530

    added python

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 # This file is used to register all your cloud functions in GCP.
 # DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
 
+from hiru-1126-gcp.python import handler as hiru1126Gcppython
